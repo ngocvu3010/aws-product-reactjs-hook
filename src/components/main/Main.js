afc42364631f49e507335b36603d066551fd2daf
Ngoc Vu <SUN-ASTERISK\vu.thi.ngoc@b120484-lt.sun-asterisk.com>
2022-10-31 16:23:36 +0700

    Pagination and sort, search branch

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Product from './Product';
+import Pagination from './Pagination';
+import TopBar from './TopBar';
 
 function Main(props) {
   const {inputSearch, selectedBranchIds, selectedTypeIds} = props;
   const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [sortType, setSortType] = useState('asc');
 
   useEffect(() => {
     let url=`http://localhost:3000/products?`;
@@ -13,14 +17,18 @@ function Main(props) {
 
     if (selectedBranchIds.length > 0) {
       for(let i in selectedBranchIds) {
-        url += `&branch_id=${selectedBranchIds[i]}`
+        url += `&branch_id=${selectedBranchIds[i]}`;
       }
     }
 
     if (selectedTypeIds.length > 0) {
       for(let i in selectedTypeIds) {
-        url += `&type_id=${selectedTypeIds[i]}`
+        url += `&type_id=${selectedTypeIds[i]}`;
       }
+    }
+
+    if (sortType) {
+      url += `&_sort=price&_order=${sortType}`;
     }
 
     fetch(url)
@@ -28,25 +36,30 @@ function Main(props) {
       .then((result) => {
         setProducts(result);
       })
-  }, [inputSearch, selectedBranchIds, selectedTypeIds]);
+  }, [inputSearch, selectedBranchIds, selectedTypeIds, sortType]);
+
+  const handlePagination = (page) => {
+    if (page < 1 || page > Math.ceil(products.length / 2)) {
+      return;
+    }
+    setCurrentPage(page);
+  }
+  const indexOfLastProduct = currentPage * 2;
+  const indexOfFirstProduct = indexOfLastProduct - 2;
+
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
 
   return(
     <div className="results-wrapper">
-      <section id="results-topbar">
-        <div className="sort-by">
-          <label>Sort by</label>
-          <div id="sort-by-selector" />
-        </div>
-        <div id="stats" className="text-muted" />
-      </section>
+      <TopBar sortType={sortType} setSortType={setSortType} products={products} />
       <main id="hits">
         {
-          products.map((product, i) => (
+          currentProducts.map((product, i) => (
             <Product product={product} key={i} />
           ))
         }
       </main>
-      <section id="pagination" />
+      <Pagination currentPage={currentPage} handlePagination={handlePagination} totalProduct={products.length} />
     </div>
   );
 }
